78b4f1085b8d84c2dcc0483a5243bfb348b863ce
geethasamynathan <geethasamynathan2011@gmail.com>
2021-11-15 10:34:11 +0530

    Validationdemo

---
+++ ValidationDemo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ValidationDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [RequireHttps]
+        public string Login()
+        {
+            return " this psge is accessed only by https protocol";
+        }
+    }
+}
---
+++ ValidationDemo/Models/EmployeeMetaData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidationDemo.Models
+{
+    [MetadataType(typeof(EmployeeMetaData))]
+    public partial class Employee { }
+
+    public class EmployeeMetaData
+    {
+     [Required(ErrorMessage =" Enter the Name")]
+     [StringLength(10,MinimumLength =5)]
+        public string Name { get; set; }
+
+        [Range(20,60)]
+        public int Age { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Range(20000,200000)]
+        [DataType(DataType.Currency)]
+        public int Salary { get; set; }
+       // public string Location { get; set; }
+    }
+}
