4d39b99a1c3156785943bec831b7586a1b6d778a
geethasamynathan <geethasamynathan2011@gmail.com>
2021-11-23 21:15:25 -0800

    Add files via upload

---
+++ consume web api/MVC Project/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using ConsumewebApiinMvcdemo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsumewebApiinMvcdemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        IEnumerable<Employee> Employees = null;
+        // GET: Employee
+        public async Task<ActionResult> Index()
+        {
+            Employees = new List<Employee>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44311");
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("/api/Employees/GetEmployees");
+           
+               
+                if (response.IsSuccessStatusCode)
+                {
+                    var resultResponse = response.Content.ReadAsStringAsync().Result;
+                    Employees = JsonConvert.DeserializeObject<List<Employee>>(resultResponse);
+                }
+                else
+                {
+                    Employees = Enumerable.Empty<Employee>();
+                    ModelState.AddModelError(string.Empty, "Server Error try afer come time");
+                }
+            }
+            return View(Employees);
+        }
+    }
+}
---
+++ consume web api/MVC Project/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using ConsumewebApiinMvcdemo.Models;
+using Newtonsoft.Json;
+
+namespace ConsumewebApiinMvcdemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        IEnumerable<Employee> Employees = null;
+        public ActionResult Index()
+        {
+           return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
